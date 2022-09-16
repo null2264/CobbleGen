99ef2607c9746c07f093bbf9e24f7681c536be07
ziro <palembani@gmail.com>
2022-09-16 17:21:11 +0700

    + Filter null when creating new config file

--- src/main/java/io/github/null2264/cobblegen/config/ConfigHelper.java
+++ src/main/java/io/github/null2264/cobblegen/config/ConfigHelper.java
@@ -1,12 +1,11 @@
 package io.github.null2264.cobblegen.config;
 
-import blue.endless.jankson.Jankson;
-import blue.endless.jankson.JsonGrammar;
-import blue.endless.jankson.JsonObject;
+import blue.endless.jankson.*;
 import blue.endless.jankson.api.SyntaxError;
 import com.google.gson.Gson;
 import net.fabricmc.loader.api.FabricLoader;
 
+import javax.annotation.Nullable;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -40,6 +39,30 @@ public class ConfigHelper {
         }
     }
 
+    /**
+     * @deprecated Removed when Jankson released their proper null filter
+     */
+    @Deprecated
+    @Nullable
+    private static JsonElement filter(JsonElement json) {
+        JsonElement result = null;
+        if (json instanceof JsonObject finalResult) {
+            finalResult.keySet().forEach(key -> {
+                JsonElement element = finalResult.get(key);
+                if (!(element instanceof JsonNull) && element != null) filter(element);
+                else finalResult.remove(key);
+            });
+            result = finalResult;
+        } else if (json instanceof JsonArray finalResult) {
+            finalResult.forEach(element -> {
+                if (element instanceof JsonObject)
+                    filter(element);
+            });
+            result = finalResult;
+        }
+        return result;
+    }
+
     public static void load() throws SyntaxError, IOException {
         try {
             LOGGER.info("Trying to (re)load config file...");
@@ -55,9 +78,11 @@ public class ConfigHelper {
         try {
             LOGGER.info("Trying to create config file...");
             FileWriter fw = new FileWriter(configFile);
-            fw.write(Jankson.builder().build().toJson(CONFIG).toJson(JsonGrammar.JSON5));
+            JsonElement jsonElement = Jankson.builder().build().toJson(CONFIG);
+            JsonElement filteredElement = filter(jsonElement);
+            fw.write((filteredElement != null ? filteredElement : jsonElement).toJson(JsonGrammar.JSON5));
             fw.close();
-        } catch (Exception e) {
+        } catch (IOException e) {
             LOGGER.error("There was an error while creating the config file!", e);
         }
     }
