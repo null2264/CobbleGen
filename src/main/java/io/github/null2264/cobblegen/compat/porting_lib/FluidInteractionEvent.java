a14f30a64b2536142958bc14b150908bc3ebbf5e
Ahmad Ansori Palembani <46041660+null2264@users.noreply.github.com>
2023-04-24 17:28:56 +0700

    fix: Porting Lib compatibility issue (#22)
    * feat: Fix compatibility with Porting Lib
    
    * chore: Exit early for obsidian
    
    * feat: Handle basalt generators
    
    * fix: Basalt generated without modifier
    
    * chore: Fix potential conflict
    
    * fix: Accidentally overwrite Create's fluid interaction
    
    * chore: Run with create properties
    
    * fix: Crash when loaded without Create/Porting Lib mod

---
+++ src/main/java/io/github/null2264/cobblegen/compat/porting_lib/FluidInteractionEvent.java
@@ -0,0 +1,63 @@
+package io.github.null2264.cobblegen.compat.porting_lib;
+
+import io.github.fabricators_of_create.porting_lib.event.common.FluidPlaceBlockCallback;
+import io.github.null2264.cobblegen.util.BlockGenerator;
+import io.github.null2264.cobblegen.util.GeneratorType;
+import lombok.val;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.LavaFluid;
+import net.minecraft.fluid.WaterFluid;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+
+import static net.minecraft.client.render.WorldRenderer.DIRECTIONS;
+
+public class FluidInteractionEvent
+{
+    public static BlockState whenFluidsMeet(WorldAccess world, BlockPos pos, BlockState state) {
+        FluidState fluidState = state.getFluidState();
+        if (fluidState.isEmpty() || (fluidState.isStill() && fluidState.isOf(Fluids.LAVA))) return null;
+
+        FluidState fluidStateAbove = world.getFluidState(pos.up());
+        if (fluidState.getFluid() instanceof WaterFluid && fluidStateAbove.getFluid() instanceof LavaFluid) {
+            val generator = new BlockGenerator(
+                    (World) world,
+                    pos,
+                    GeneratorType.STONE
+            );
+            return generator.getReplacement();
+        }
+
+        for (Direction direction : DIRECTIONS) {
+            if (direction == Direction.UP) continue;
+
+            FluidState metFluidState = fluidState.isStill() ? fluidState : world.getFluidState(pos.offset(direction));
+            BlockPos blockPos = pos.offset(direction.getOpposite());
+            if (metFluidState.getFluid() instanceof WaterFluid) {
+                val generator = new BlockGenerator(
+                        (World) world,
+                        pos,
+                        GeneratorType.COBBLE
+                );
+                return generator.getReplacement();
+            } else if (world.getBlockState(blockPos).isOf(Blocks.BLUE_ICE)) {
+                val generator = new BlockGenerator((World) world, pos, GeneratorType.BASALT);
+                return generator.getReplacement();
+            }
+        }
+        return null;
+    }
+
+    public static void register() {
+        FluidPlaceBlockCallback.EVENT.register(FluidInteractionEvent::whenFluidsMeet);
+    }
+
+    public static BlockState invoke(WorldAccess world, BlockPos pos, BlockState state) {
+        return FluidPlaceBlockCallback.EVENT.invoker().onFluidPlaceBlock(world, pos, state);
+    }
+}
--- src/main/java/io/github/null2264/cobblegen/mixin/FluidEventMixin.java
+++ src/main/java/io/github/null2264/cobblegen/mixin/FluidEventMixin.java
@@ -1,8 +1,10 @@
 package io.github.null2264.cobblegen.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import io.github.null2264.cobblegen.compat.porting_lib.FluidInteractionEvent;
 import io.github.null2264.cobblegen.util.BlockGenerator;
 import io.github.null2264.cobblegen.util.GeneratorType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FluidBlock;
@@ -19,12 +21,21 @@ public abstract class FluidEventMixin
 {
     private BlockState basaltReplacement;
 
-    private boolean shouldBasaltGenerate(World world, BlockPos pos) {
-        BlockGenerator basaltGenerator = new BlockGenerator(world, pos, GeneratorType.BASALT);
-        basaltReplacement = basaltGenerator.getReplacement();
-        return basaltReplacement != null;
+    private boolean shouldBasaltGenerate(World world, BlockPos pos, BlockState state, Boolean canGenerate) {
+        if (FabricLoader.getInstance().isModLoaded("porting_lib") && canGenerate)
+            basaltReplacement = FluidInteractionEvent.invoke(world, pos, state);
+
+        if (basaltReplacement == null) {
+            BlockGenerator basaltGenerator = new BlockGenerator(world, pos, GeneratorType.BASALT);
+            basaltReplacement = basaltGenerator.getReplacement();
+        }
+        return basaltReplacement != null || canGenerate;
     }
 
+    /**
+     * Handle fluid interactions (for cobblestone generator).
+     * If Porting Lib is installed, fluid interactions will be handled by {@link io.github.null2264.cobblegen.compat.porting_lib.FluidInteractionEvent}
+     */
     @ModifyArgs(method = "receiveNeighborFluids", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z", ordinal = 0))
     private void cobble$receiveNeighborFluids(
             @NotNull Args args,
@@ -32,6 +43,7 @@ public abstract class FluidEventMixin
             BlockPos pos,
             BlockState fluidBlockState
     ) {
+        if (FabricLoader.getInstance().isModLoaded("porting_lib")) return;
         if (((BlockState) args.get(1)).isOf(Blocks.OBSIDIAN)) return;
 
         BlockGenerator generator = new BlockGenerator(world, pos, GeneratorType.COBBLE);
@@ -43,15 +55,16 @@ public abstract class FluidEventMixin
             boolean original,
             World world,
             BlockPos pos,
-            BlockState ignoredState
+            BlockState state
     ) {
-        return shouldBasaltGenerate(world, pos) || original;
+        return shouldBasaltGenerate(world, pos, state, original);
     }
 
     @ModifyArgs(method = "receiveNeighborFluids", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z", ordinal = 1))
     private void basalt$receiveNeighborFluids(Args args, World world, BlockPos pos, BlockState fluidBlockState) {
-        BlockState replacement = basaltReplacement;
-
-        if (replacement != null) args.set(1, replacement);
+        if (basaltReplacement != null) {
+            args.set(1, basaltReplacement);
+            basaltReplacement = null;
+        }
     }
 }
