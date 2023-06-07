7fb4056637843f3ecae46cb2bb414cb145a3f48e
ziro <palembani@gmail.com>
2023-06-07 09:58:30 +0700

    chore: Initial ReplayMod Preprocessor multi-version multi-loader implementation

---
+++ root.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("xyz.deftu.gradle.multiversion-root")
+}
+
+preprocess {
+    val fabric12000 = createNode("1.20-fabric", 12000, "yarn")
+    val fabric11904 = createNode("1.19.4-fabric", 11904, "yarn")
+    val fabric11903 = createNode("1.19.3-fabric", 11903, "yarn")
+    val fabric11902 = createNode("1.19.2-fabric", 11902, "yarn")
+    val fabric11802 = createNode("1.18.2-fabric", 11802, "yarn")
+
+    fabric12000.link(fabric11904)
+    fabric11904.link(fabric11903)
+    fabric11903.link(fabric11902)
+    fabric11902.link(fabric11802)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.architectury.dev/")
+        maven("https://maven.deftu.xyz/releases")
+        maven("https://files.minecraftforge.net/maven/")
+        maven("https://repo.essential.gg/repository/maven-public")
+        gradlePluginPortal()
+    }
+    plugins {
+        id("xyz.deftu.gradle.multiversion-root") version("1.17.0")
+    }
+}
+
+rootProject.name = "CobbleGen"
+rootProject.buildFileName = "root.gradle.kts"
+
+listOf(
+    "1.18.2-fabric",
+    "1.18.2-forge",
+    "1.19-fabric",
+    "1.19.1-fabric",
+    "1.19.2-fabric",
+    "1.19.3-fabric",
+    "1.19.4-fabric",
+    "1.20-fabric",
+).forEach { version ->
+    include(":$version")
+    project(":$version").apply {
+        projectDir = file("versions/$version")
+        buildFileName = "../../build.gradle"
+    }
+}
