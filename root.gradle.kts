5a1fdb3a312a54f2d9a4d4acc17a8df524873fed
ziro <palembani@gmail.com>
2023-06-15 16:43:40 +0700

    fix: Fork Preprocessor

--- root.gradle.kts
+++ root.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
-    id("xyz.deftu.gradle.preprocess")
+    id("com.github.null2264.preprocess")
 }
 
 preprocess {
-    val fabric12001 = createNode("1.20.1-fabric", 12001, "mojmap")
-    val fabric11904 = createNode("1.19.4-fabric", 11904, "mojmap")
-    val fabric11902 = createNode("1.19.2-fabric", 11902, "mojmap")
-    val fabric11802 = createNode("1.18.2-fabric", 11802, "mojmap")
-    val forge11802 = createNode("1.18.2-forge", 11802, "mojmap")
+    val fabric12001 = createNode("1.20.1-fabric", 12001, "mojang")
+    val fabric11904 = createNode("1.19.4-fabric", 11904, "mojang")
+    val fabric11902 = createNode("1.19.2-fabric", 11902, "mojang")
+    val fabric11802 = createNode("1.18.2-fabric", 11802, "mojang")
+    val forge11802 = createNode("1.18.2-forge", 11802, "mojang")
 
     fabric12001.link(fabric11904)
     fabric11904.link(fabric11902)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,19 @@ pluginManagement {
     repositories {
         maven("https://maven.fabricmc.net/")
         maven("https://maven.architectury.dev/")
-        maven("https://maven.deftu.xyz/releases")
+        maven("https://jitpack.io/")
         maven("https://files.minecraftforge.net/maven/")
-        maven("https://repo.essential.gg/repository/maven-public")
+        maven("https://repo.essential.gg/repository/maven-public/")
         gradlePluginPortal()
     }
     plugins {
         id("com.github.johnrengelman.shadow") version("8.1.1")
-        id("xyz.deftu.gradle.preprocess") version("0.1.0")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.github.null2264.preprocess")
+                useModule("com.github.null2264:Preprocessor:ba61239")
+        }
     }
 }
 
