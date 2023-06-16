98c9d9a165c0814942ccb0a91e789ce376ebc3a1
ziro <palembani@gmail.com>
2023-06-16 07:17:01 +0700

    fix: PlayerManagerMixin

--- src/main/java/io/github/null2264/cobblegen/mixin/network/PlayerManagerMixin.java
+++ src/main/java/io/github/null2264/cobblegen/mixin/network/PlayerManagerMixin.java
@@ -1,7 +1,6 @@
 package io.github.null2264.cobblegen.mixin.network;
 
 import io.github.null2264.cobblegen.network.CGServerPlayNetworkHandler;
-//import net.fabricmc.api.Environment;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.players.PlayerList;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,16 +8,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-//import static net.fabricmc.api.EnvType.SERVER;
-
-//@Environment(SERVER)
 @Mixin(PlayerList.class)
 public abstract class PlayerManagerMixin
 {
     @Inject(
             method = "reloadResources",
-            at = @At(value = "HEAD")
-            //at = @At(value = "INVOKE", target = "net/minecraft/network/protocol/game/ClientboundUpdateTagsPacket.<init>(Ljava/util/Map;)V")
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastAll(Lnet/minecraft/network/protocol/Packet;)V")
     )
     public void syncOnReload(CallbackInfo ci) {
         //noinspection DataFlowIssue
