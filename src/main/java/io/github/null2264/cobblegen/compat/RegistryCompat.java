77de013f336b690339c2f643a20700d8d4e8f63c
ziro <palembani@gmail.com>
2023-06-16 17:38:45 +0700

    fix: 1.19.3/4 breaking changes

--- src/main/java/io/github/null2264/cobblegen/compat/RegistryCompat.java
+++ src/main/java/io/github/null2264/cobblegen/compat/RegistryCompat.java
@@ -1,12 +1,16 @@
 package io.github.null2264.cobblegen.compat;
 
-import net.minecraft.core.DefaultedRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Fluid;
 
 public class RegistryCompat {
-    public static DefaultedRegistry<Fluid> fluid() {
+    // for some reason DefaultedRegistry turned into DefaultedMappedRegistry, that's why it's like this
+    //#if MC<=11902
+    public static net.minecraft.core.DefaultedRegistry<Fluid> fluid() {
+    //#else
+    //$$ public static net.minecraft.core.DefaultedRegistry<Fluid> fluid() {
+    //#endif
         //#if MC<=11902
         return Registry.FLUID;
         //#else
@@ -14,7 +18,11 @@ public class RegistryCompat {
         //#endif
     }
 
-    public static DefaultedRegistry<Block> block() {
+    //#if MC<=11902
+    public static net.minecraft.core.DefaultedRegistry<Block> block() {
+    //#else
+    //$$ public static net.minecraft.core.DefaultedRegistry<Block> block() {
+    //#endif
         //#if MC<=11902
         return Registry.BLOCK;
         //#else
--- src/main/java/io/github/null2264/cobblegen/mixin/network/ServerPacketListenerMixin.java
+++ src/main/java/io/github/null2264/cobblegen/mixin/network/ServerPacketListenerMixin.java
@@ -3,9 +3,12 @@ package io.github.null2264.cobblegen.mixin.network;
 import io.github.null2264.cobblegen.network.CGServerPlayNetworkHandler;
 import io.github.null2264.cobblegen.util.Util;
 import lombok.val;
+import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,6 +16,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerGamePacketListenerImpl.class)
 public abstract class ServerPacketListenerMixin
 {
+    @Shadow
+    @Final
+    private Connection connection;
+
     @SuppressWarnings("DataFlowIssue")
     private ServerGamePacketListenerImpl getListener() {
         return (ServerGamePacketListenerImpl) (Object) this;
@@ -24,7 +31,7 @@ public abstract class ServerPacketListenerMixin
         //#if FABRIC>=1
         if (Util.isPortingLibLoaded()) {
             // Just in case
-            if (self.connection instanceof io.github.fabricators_of_create.porting_lib.fake_players.FakeConnection) return;
+            if (this.connection instanceof io.github.fabricators_of_create.porting_lib.fake_players.FakeConnection) return;
         }
         //#endif
         CGServerPlayNetworkHandler.trySync(self);
