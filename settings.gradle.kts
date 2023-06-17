0be79079251d3887549e09c0efe25d11146e7ff0
ziro <palembani@gmail.com>
2023-06-17 13:02:59 +0700

    feat: Add 1.19.3+ and 1.20.x Forge support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,9 @@ listOf(
     "1.19.2-fabric",
     "1.19.2-forge",
     "1.19.4-fabric",
+    "1.19.4-forge",
     "1.20.1-fabric",
+    "1.20.1-forge",
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
