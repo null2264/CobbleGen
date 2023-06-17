b3070e71dad70f407b23181c2edd67de113c68fb
ziro <palembani@gmail.com>
2023-06-17 07:44:18 +0700

    fix: 1.19.2 forge "fluid overhaul" completely overwrite vanilla's fluid behaviour

--- src/main/java/io/github/null2264/cobblegen/mixin/fluid/FluidEventMixin.java
+++ src/main/java/io/github/null2264/cobblegen/mixin/fluid/FluidEventMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static io.github.null2264.cobblegen.CobbleGen.FLUID_INTERACTION;
@@ -22,16 +23,59 @@ public abstract class FluidEventMixin
     @Final
     protected FlowingFluid fluid;
 
+    @SuppressWarnings("unused")
+    private void doInteraction(Level level, BlockPos pos, BlockState state, CallbackInfo ci) {
+        val success = FLUID_INTERACTION.interact(level, pos, state);
+
+        if (success)
+            ci.cancel();
+    }
+
+    private void doInteraction(Level level, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> cir) {
+        val success = FLUID_INTERACTION.interact(level, pos, state);
+
+        if (success)
+            cir.setReturnValue(false);
+    }
+
+    //#if MC>=11900 && FABRIC<=0
+    //$$ @Inject(method = "onPlace", at = @At("HEAD"), cancellable = true)
+    //$$ private void fluidInteraction$onPlace(
+    //$$         BlockState state,
+    //$$         Level level,
+    //$$         BlockPos pos,
+    //$$         BlockState blockState2,
+    //$$         boolean bl,
+    //$$         CallbackInfo ci
+    //$$ ) {
+    //$$     // Forge 1.19.x completely changed Vanilla fluid behaviour
+    //$$     // REF: https://github.com/MinecraftForge/MinecraftForge/pull/8695
+    //$$     doInteraction(level, pos, state, ci);
+    //$$ }
+    //$$
+    //$$ @Inject(method = "neighborChanged", at = @At("HEAD"), cancellable = true)
+    //$$ private void fluidInteraction$neighborChangede(
+    //$$         BlockState state,
+    //$$         Level level,
+    //$$         BlockPos pos,
+    //$$         net.minecraft.world.level.block.Block block,
+    //$$         BlockPos blockPos2,
+    //$$         boolean bl,
+    //$$         CallbackInfo ci
+    //$$ ) {
+    //$$     // Forge 1.19.x completely changed Vanilla fluid behaviour
+    //$$     // REF: https://github.com/MinecraftForge/MinecraftForge/pull/8695
+    //$$     doInteraction(level, pos, state, ci);
+    //$$ }
+    //#endif
+
     @Inject(method = "shouldSpreadLiquid", at = @At("HEAD"), cancellable = true)
     private void fluidInteraction$shouldSpreadLiquids(
-            Level world,
+            Level level,
             BlockPos pos,
             BlockState state,
             CallbackInfoReturnable<Boolean> cir
     ) {
-        val success = FLUID_INTERACTION.interact(world, pos, state);
-
-        if (success)
-            cir.setReturnValue(false);
+        doInteraction(level, pos, state, cir);
     }
 }
