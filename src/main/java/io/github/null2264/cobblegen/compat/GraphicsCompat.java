e0c7caa889799278dc1964d58b230e8f2681aeff
ziro <palembani@gmail.com>
2023-06-19 08:10:22 +0700

    fix: Wrong operator

--- src/main/java/io/github/null2264/cobblegen/compat/GraphicsCompat.java
+++ src/main/java/io/github/null2264/cobblegen/compat/GraphicsCompat.java
@@ -7,13 +7,13 @@ import net.minecraft.network.chat.Component;
 
 public class GraphicsCompat {
     @SuppressWarnings("UnusedReturnValue")
-    //#if MC<=12000
+    //#if MC<12000
     public static int drawString(PoseStack pose, Component text, int x, int y, int colour) {
     //#else
     //$$ public static int draw(net.minecraft.client.gui.GuiGraphics graphics, Component text, int x, int y, int colour) {
     //#endif
         Font font = Minecraft.getInstance().font;
-        //#if MC<=12000
+        //#if MC<12000
         return font.draw(pose, text, x, y, colour);
         //#else
         //$$ return graphics.drawString(font, pose, text, x, y, colour, false);
