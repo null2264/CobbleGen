ac721ff10a1b5e6976edff61a274895ba70dc045
ziro <palembani@gmail.com>
2023-09-21 13:10:08 +0700

    chore: 1.20.2 support Testing Minecraft's Configuration Phase (ServerConfigurationNetworking)
    REF: https://fabricmc.net/2023/09/12/1202.html#networking

--- root.gradle.kts
+++ root.gradle.kts
@@ -3,6 +3,8 @@ plugins {
 }
 
 preprocess {
+    //val forge12002 = createNode("1.20.2-forge", 12002, "mojang")
+    val fabric12002 = createNode("1.20.2-fabric", 12002, "mojang")
     val forge12001 = createNode("1.20.1-forge", 12001, "mojang")
     val fabric12001 = createNode("1.20.1-fabric", 12001, "mojang")
     val forge11904 = createNode("1.19.4-forge", 11904, "mojang")
@@ -12,6 +14,8 @@ preprocess {
     val forge11802 = createNode("1.18.2-forge", 11802, "mojang")
     val fabric11802 = createNode("1.18.2-fabric", 11802, "mojang")
 
+    //forge12002.link(fabric12002)
+    fabric12002.link(fabric12001)
     forge12001.link(fabric12001)
     fabric12001.link(fabric11904)
     forge11904.link(fabric11904)
