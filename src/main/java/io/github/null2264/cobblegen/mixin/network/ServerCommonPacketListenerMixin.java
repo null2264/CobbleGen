dac6d4ca20f7cf83b2765145c9347507fdbff7f7
ziro <palembani@gmail.com>
2023-09-23 14:43:09 +0700

    fix: Accidentally remove fabric checker

--- src/main/java/io/github/null2264/cobblegen/mixin/network/ServerCommonPacketListenerMixin.java
+++ src/main/java/io/github/null2264/cobblegen/mixin/network/ServerCommonPacketListenerMixin.java
@@ -40,10 +40,12 @@ public abstract class ServerCommonPacketListenerMixin
     @Inject(method = "<init>", at = @At("RETURN"))
     private void init(CallbackInfo ci) {
         val self = getListener();
+        //#if FABRIC>=1
         if (Util.isPortingLibLoaded()) {
             // Just in case
             if (this.connection instanceof io.github.fabricators_of_create.porting_lib.fake_players.FakeConnection) return;
         }
+        //#endif
         CGServerPlayNetworkHandler.trySync(self);
     }
 
