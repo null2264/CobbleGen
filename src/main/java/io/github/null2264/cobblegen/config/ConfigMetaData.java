7c3db062d2917a3848d169b33950db3746ba8424
ziro <palembani@gmail.com>
2023-11-12 08:31:14 +0700

    feat: Add more option to meta config

--- src/main/java/io/github/null2264/cobblegen/config/ConfigMetaData.java
+++ src/main/java/io/github/null2264/cobblegen/config/ConfigMetaData.java
@@ -1,12 +1,30 @@
 package io.github.null2264.cobblegen.config;
 
 import blue.endless.jankson.Comment;
+import org.jetbrains.annotations.NotNull;
 
 public class ConfigMetaData implements Config
 {
+    @Comment(value="Enable Recipe Viewer support (EMI/REI/JEI)")
+    @NotNull
+    public Boolean enableRecipeViewer = true;
+    
     @Comment(value="Enable Experimental Features")
+    @NotNull
     public Boolean enableExperimentalFeatures = false;
 
     @Comment(value="Merge CobbleGen recipe categories into EMI's World Interaction category")
+    @NotNull
     public Boolean mergeEMIRecipeCategory = true;
+
+    @Comment(value="EMI related config, used when mergeEMIRecipeCategory is set to 'true'")
+    public EMIData emi = new EMIData();
+
+    public static class EMIData {
+        @Comment(value="Add CobbleGen tooltip when CobbleGen is merged with EMI's World Interaction category")
+        public Boolean addTooltip = true;
+
+        @Comment(value="Remove overlapping recipe between CobbleGen and EMI when CobbleGen is merged with EMI's World Interaction category")
+        public Boolean removeOverlaps = true;
+    }
 }
