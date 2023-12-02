778397b5c6ee013b2aa4c61df1f139cc1ea67472
Ahmad Ansori Palembani <46041660+null2264@users.noreply.github.com>
2023-12-02 16:17:32 +0700

    fix: GH-55 - Incompatibility with FAPI 0.91+ (GH-56)
    * fix: Catch NoClassDefFoundError on FAPI 0.91+
    
    * chore: Simplify Create Mixin
    
    * fix: Never use FAPI's PacketByteBufPayload

--- src/main/java/io/github/null2264/cobblegen/mixin/network/ServerboundCustomPayloadPacketMixin.java
+++ src/main/java/io/github/null2264/cobblegen/mixin/network/ServerboundCustomPayloadPacketMixin.java
@@ -1,29 +1,29 @@
 package io.github.null2264.cobblegen.mixin.network;
 
-import net.minecraft.network.FriendlyByteBuf;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
 //#if MC<1.20.2
 import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
 //#else
+//$$ import net.minecraft.network.FriendlyByteBuf;
 //$$ import io.netty.buffer.Unpooled;
 //$$ import io.github.null2264.cobblegen.network.PacketByteBufPayload;
 //$$ import net.minecraft.network.protocol.common.ServerboundCustomPayloadPacket;
 //$$ import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+//$$ import net.minecraft.resources.ResourceLocation;
+//$$ import org.spongepowered.asm.mixin.injection.At;
+//$$ import org.spongepowered.asm.mixin.injection.Inject;
+//$$ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+//$$
+//$$ import static io.github.null2264.cobblegen.CobbleGen.MOD_ID;
 //#endif
-import net.minecraft.resources.ResourceLocation;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static io.github.null2264.cobblegen.CobbleGen.SYNC_CHANNEL;
-import static io.github.null2264.cobblegen.CobbleGen.SYNC_PING_CHANNEL;
-
-@Mixin(ServerboundCustomPayloadPacket.class)
+@Mixin(value = ServerboundCustomPayloadPacket.class, priority = 999)
 public abstract class ServerboundCustomPayloadPacketMixin {
     //#if MC>=1.20.2
     //$$ @Inject(method = "readPayload", at = @At("HEAD"), cancellable = true)
     //$$ private static void read(ResourceLocation id, FriendlyByteBuf buf, CallbackInfoReturnable<CustomPacketPayload> cir) {
-    //$$     if (!(id.equals(SYNC_CHANNEL)) || (id.equals(SYNC_PING_CHANNEL)))
+    //$$     if (!id.getNamespace().equals(MOD_ID))
     //$$         return;
     //$$
     //$$     FriendlyByteBuf newBuf = new FriendlyByteBuf(Unpooled.buffer());
