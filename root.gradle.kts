4502ecfd4223a57f7336b2794b38afef34cdb65f
ziro <palembani@gmail.com>
2023-12-05 06:31:12 +0700

    chore: Drop support for Forge (1.20.2)

--- root.gradle.kts
+++ root.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 
 preprocess {
     val neo12002 = createNode("1.20.2-neoforge", 12002, "mojang")
-    val forge12002 = createNode("1.20.2-forge", 12002, "mojang")
     val fabric12002 = createNode("1.20.2-fabric", 12002, "mojang")
     val forge12001 = createNode("1.20.1-forge", 12001, "mojang")
     val fabric12001 = createNode("1.20.1-fabric", 12001, "mojang")
@@ -15,7 +14,6 @@ preprocess {
 
     neo12002.link(fabric12002)
 
-    forge12002.link(fabric12002)
     forge12001.link(fabric12001)
     forge11902.link(fabric11902)
     forge11802.link(fabric11802)
