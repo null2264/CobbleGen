9ddb6a3ea5b7adac78acbe1d0f6f2a2561a825eb
ziro <palembani@gmail.com>
2023-12-07 12:16:48 +0700

    docs: Add @since to shouldLoad javaDoc

--- src/main/java/io/github/null2264/cobblegen/CobbleGenPlugin.java
+++ src/main/java/io/github/null2264/cobblegen/CobbleGenPlugin.java
@@ -1,6 +1,7 @@
 package io.github.null2264.cobblegen;
 
 import io.github.null2264.cobblegen.data.model.CGRegistry;
+import org.jetbrains.annotations.ApiStatus;
 
 public interface CobbleGenPlugin
 {
@@ -18,7 +19,9 @@ public interface CobbleGenPlugin
     /**
      * Should this fluid interaction be loaded
      * @return true if it should be loaded
+     * @since CobbleGen v5.3.0
      */
+    @ApiStatus.AvailableSince("5.3.0")
     default boolean shouldLoad() {
         return true;
     }
