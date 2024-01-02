8dc622a8a2334c2680e7faa84f3d8079e51b0465
ziro <palembani@gmail.com>
2024-01-02 15:24:39 +0700

    fix: Incorrect import position

--- src/main/java/io/github/null2264/cobblegen/mixin/network/ServerboundCustomPayloadPacketAccessor.java
+++ src/main/java/io/github/null2264/cobblegen/mixin/network/ServerboundCustomPayloadPacketAccessor.java
@@ -1,14 +1,15 @@
 package io.github.null2264.cobblegen.mixin.network;
 
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.resources.ResourceLocation;
-import org.spongepowered.asm.mixin.Mixin;
 //#if MC<1.20.2
 import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
-import org.spongepowered.asm.mixin.gen.Accessor;
 //#else
 //$$ import net.minecraft.network.protocol.common.ServerboundCustomPayloadPacket;
 //#endif
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(ServerboundCustomPayloadPacket.class)
 public interface ServerboundCustomPayloadPacketAccessor {
