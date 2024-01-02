b29622c4c8b41421d66de62a700113ba12dbbe8e
ziro <palembani@gmail.com>
2024-01-02 15:56:43 +0700

    fix: 1.20.2 don't need it

--- src/main/java/io/github/null2264/cobblegen/mixin/network/ServerboundCustomPayloadPacketAccessor.java
+++ src/main/java/io/github/null2264/cobblegen/mixin/network/ServerboundCustomPayloadPacketAccessor.java
@@ -1,12 +1,8 @@
+//#if MC<1.20.2
 package io.github.null2264.cobblegen.mixin.network;
 
-//#if MC<1.20.2
-import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
-//#else
-//$$ import net.minecraft.network.protocol.common.ServerboundCustomPayloadPacket;
-//#endif
-
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
@@ -19,3 +15,4 @@ public interface ServerboundCustomPayloadPacketAccessor {
     @Accessor("data")
     FriendlyByteBuf getByte();
 }
+//#endif
