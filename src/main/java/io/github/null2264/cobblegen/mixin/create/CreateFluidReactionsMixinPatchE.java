c66f3a93da78f7b7c58f3998dca56f0cc05555b7
Ahmad Ansori Palembani <46041660+null2264@users.noreply.github.com>
2024-03-17 12:35:54 +0700

    fix: Create Fabric Patch F incompatibility (#63)
    * chore: Drop Create 0.5.0 support
    
    * fix: Patch F breaking changes
    
    * revert: Drop 0.5.0 support
    
    Doesn't seem necessary to drop it
    
    * fix: Patch E mixin should always loaded on Forge while Patch F mixin should only load on Fabric
    
    * fix: Completely forgot about MC1.16.5 support
    
    * fix: More MC 1.16 stuff
    
    * fix: I don't even know anymore
    
    * fix: Surely this works
    
    * chore: Update manifold
    
    * docs: Some comments
    
    * fix: Log4J nonsense
    
    * refactor: Adjust shadow relocation
    
    * ci: Bump CI action version
    
    * revert: "refactor: Adjust shadow relocation"
    
    This reverts commit 84f16e39eb396d0a48dd9d5fe37422efc1b6bce4.
    
    * revert: "fix: Log4J nonsense"
    
    This reverts commit b5ed016a68b6309cdf3b594002250a55e10c9eb4.
    
    * refactor: Adjust shadow relocation
    
    * fix: Don't include log4j for 1.18.2
    
    * ci: Update actions
    
    * fix: Ability to disable pipe integration
    
    * refactor: Flip 0 and 1
    
    * fix: Note to self, mixinClassName may include full package path
    
    * fix: Race condition

---
+++ src/main/java/io/github/null2264/cobblegen/mixin/create/CreateFluidReactionsMixinPatchE.java
@@ -0,0 +1,46 @@
+//#if MC>1.16.5
+package io.github.null2264.cobblegen.mixin.create;
+
+//#if FABRIC>=1
+import io.github.fabricators_of_create.porting_lib.util.FluidStack;
+//#else
+    //#if FORGE>=2 && MC>=1.20.2
+    //$$ import net.neoforged.neoforge.fluids.FluidStack;
+    //#else
+    //$$ import net.minecraftforge.fluids.FluidStack;
+    //#endif
+//#endif
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static io.github.null2264.cobblegen.CobbleGen.FLUID_INTERACTION;
+
+/**
+ * Mixin for Create Fabric (pre) Patch F
+ */
+@Pseudo
+@Mixin(targets = {"com.simibubi.create.content.contraptions.fluids.FluidReactions", "com.simibubi.create.content.fluids.FluidReactions"})
+public abstract class CreateFluidReactionsMixinPatchE
+{
+    @Inject(
+        method = "handlePipeFlowCollision",
+        //#if FABRIC<=0
+        //$$ remap = false,
+        //#endif
+        at = @At(value = "HEAD"), cancellable = true
+    )
+    private static void generator$handlePipeFlowCollision(
+        Level level, BlockPos pos, FluidStack fluid1, FluidStack fluid2, CallbackInfo ci
+    ) {
+        final boolean success = FLUID_INTERACTION.interactFromPipe(level, pos, fluid1.getFluid(), fluid2.getFluid());
+        if (success)
+            ci.cancel();
+    }
+}
+//#endif
