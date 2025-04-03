f92274575a5985ebad244195e5f44725d25a47c3
Ahmad Ansori Palembani <palembani@gmail.com>
2025-04-03 11:01:15 +0700

    fix: 1.20.2 accidentally marked as unsupported

--- buildSrc/src/main/kotlin/dependencies/RecipeViewer.kt
+++ buildSrc/src/main/kotlin/dependencies/RecipeViewer.kt
@@ -15,7 +15,7 @@ fun emi(loader: String? = null, api: Boolean = false) = Dependency(
                         in 11900..11902 -> "1.19.2"
                         in 11903..11904 -> versionStr(mcVersion)
                         in 12000..12001 -> "1.20.1"
-                        12002 or 12004 -> "1.20.4"
+                        12002, 12004 -> versionStr(mcVersion)
                         12003 -> "1.20.2"
                         in 12005..12006 -> "1.20.6"
                         in 12100..12105 -> "1.21.1"
