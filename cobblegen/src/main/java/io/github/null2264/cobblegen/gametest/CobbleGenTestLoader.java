e19ef77e8537c80a71716efeb036c09a62e1733c
Ahmad Ansori Palembani <palembani@gmail.com>
2025-04-06 17:13:44 +0700

    fix: Use environment variable to enable gametest
    For whatever reason HeadlessMC refuses to hand over the -D parameter

--- cobblegen/src/main/java/io/github/null2264/cobblegen/gametest/CobbleGenTestLoader.java
+++ cobblegen/src/main/java/io/github/null2264/cobblegen/gametest/CobbleGenTestLoader.java
@@ -14,9 +14,13 @@ import java.util.function.Consumer;
 
 public class CobbleGenTestLoader extends TestFunctionLoader {
     private static final AtomicBoolean SHOULD_REGISTER = new AtomicBoolean(true);
-    public static final boolean ENABLED = boolFromString(System.getProperty("null2264.cobblegen.gametest", "false"));
+    public static final boolean ENABLED = boolFromString(
+        System.getProperty("null2264.cobblegen.gametest", System.getenv("ENABLE_NULL2264_COBBLEGEN_GAMETEST"))
+    );
 
     private static boolean boolFromString(String string) {
+        if (string == null) return false;
+
         List<String> yes = List.of("yes", "y", "true", "t", "1", "enable", "on");
         List<String> no = List.of("no", "n", "false", "f", "0", "disable", "off");
 
