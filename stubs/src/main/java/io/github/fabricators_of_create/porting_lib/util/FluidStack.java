8e0a1f3c10358feb64a339bc191b37b524b78d06
Ahmad Ansori Palembani <palembani@gmail.com>
2025-07-01 10:39:26 +0700

    fix: Stop java from crying

--- stubs/src/main/java/io/github/fabricators_of_create/porting_lib/util/FluidStack.java
+++ stubs/src/main/java/io/github/fabricators_of_create/porting_lib/util/FluidStack.java
@@ -3,7 +3,7 @@ package io.github.fabricators_of_create.porting_lib.util;
 import net.minecraft.world.level.material.Fluid;
 
 public class FluidStack {
-    public Fluid getFluid() throws IllegalAccessException {
-        throw new IllegalAccessException("Stub!");
+    public Fluid getFluid() {
+        throw new RuntimeException("Stub!");
     }
 }
