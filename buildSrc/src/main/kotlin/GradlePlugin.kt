f5491fa04c1cc7a8ca3b62412661708b41de462c
Ahmad Ansori Palembani <palembani@gmail.com>
2026-01-29 12:34:57 +0700

    fix: Loom is not legacy

--- buildSrc/src/main/kotlin/GradlePlugin.kt
+++ buildSrc/src/main/kotlin/GradlePlugin.kt
@@ -1,6 +1,6 @@
 sealed class GradlePlugin(val id: String, val isLegacy: Boolean) {
     object LegacyLoom : GradlePlugin("net.fabricmc.fabric-loom-remap", true)
-    object Loom : GradlePlugin("net.fabricmc.fabric-loom", true)
+    object Loom : GradlePlugin("net.fabricmc.fabric-loom", false)
     object ArchLoom : GradlePlugin("io.github.null2264.architectury-loom", true)
     object MDG : GradlePlugin("net.neoforged.moddev", false)
     object LegacyMDG : GradlePlugin("net.neoforged.moddev.legacyforge", true)
