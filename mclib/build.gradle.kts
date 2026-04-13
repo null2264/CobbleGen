f46d5327b53508e616fb66cae668d5abccae3e9b
Ahmad Ansori Palembani <palembani@gmail.com>
2026-04-13 11:24:43 +0700

    fix: modCompileOnly is loom only

--- mclib/build.gradle.kts
+++ mclib/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     // Mostly for Mixin and Arch's dependant on Fabric's @Environment annotations
-    modCompileOnly("net.fabricmc:fabric-loader:0.17.2")
+    compileOnly("net.fabricmc:fabric-loader:0.17.2")
 }
 
 publishing {
