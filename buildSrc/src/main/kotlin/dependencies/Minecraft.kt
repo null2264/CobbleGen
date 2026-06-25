626a974faa772aac87286ea7c3389ab7215611a0
Ahmad Ansori Palembani <palembani@gmail.com>
2026-06-25 11:44:28 +0700

    fix: Old version number format for new MC version formatting

--- buildSrc/src/main/kotlin/dependencies/Minecraft.kt
+++ buildSrc/src/main/kotlin/dependencies/Minecraft.kt
@@ -6,8 +6,7 @@ val minecraft = Dependency(
     version = { mcVersion ->
         when (mcVersion) {
             // For snapshots
-            //12100 -> "some snapshot"
-            260100 -> "26.1-snapshot-1"
+            //260100 -> "26.1-snapshot-1"
             else -> versionStr(mcVersion)
         }
     },
