6eee705c8304edfa53fc927ff88a8918695c4eed
Ahmad Ansori Palembani <palembani@gmail.com>
2026-06-28 06:58:48 +0700

    revert: CT for 26.1

--- cobblegen/build.gradle.kts
+++ cobblegen/build.gradle.kts
@@ -15,9 +15,7 @@ val isNeo = ext["isNeo"] as Boolean
 group = project.properties["maven_group"] as String
 
 loom {
-    if (mcVersion >= 260100) {
-        accessWidenerPath = project.file("src/main/resources/cobblegen26.classtweaker")
-    } else if (mcVersion >= 12105) {
+    if (mcVersion >= 12105) {
         accessWidenerPath = project.file("src/main/resources/cobblegen.classtweaker")
     }
 
