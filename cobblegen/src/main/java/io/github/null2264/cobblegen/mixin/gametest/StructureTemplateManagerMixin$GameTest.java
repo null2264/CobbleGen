ea685d92950dbbb1822e9684a8fdde81f8bbcd57
Ahmad Ansori Palembani <palembani@gmail.com>
2026-08-25 22:12:09 +0700

    fix: readStructure and resourceManager is gone

--- cobblegen/src/main/java/io/github/null2264/cobblegen/mixin/gametest/StructureTemplateManagerMixin$GameTest.java
+++ cobblegen/src/main/java/io/github/null2264/cobblegen/mixin/gametest/StructureTemplateManagerMixin$GameTest.java
@@ -12,6 +12,7 @@ import net.minecraft.nbt.NbtUtils;
 import net.minecraft.resources.FileToIdConverter;
 import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.datafix.DataFixTypes;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 import net.minecraft.world.level.storage.LevelStorageSource;
@@ -30,11 +31,13 @@ import java.util.stream.Stream;
 @Mixin(StructureTemplateManager.class)
 public abstract class StructureTemplateManagerMixin$GameTest {
 
+    #if MC<260100
     @Shadow
     private ResourceManager resourceManager;
 
     @Shadow
     public abstract StructureTemplate readStructure(CompoundTag nbt);
+    #endif
 
     @Unique
     private static final FileToIdConverter GAMETEST_STRUCTURE_LISTER = new FileToIdConverter("gametest/structure", ".snbt");
@@ -48,9 +51,15 @@ public abstract class StructureTemplateManagerMixin$GameTest {
         ResourceLocation
         #endif
         id
+        #if MC>=260100
+        ,
+        ResourceManager resourceManager,
+        DataFixer dataFixer,
+        HolderGetter<net.minecraft.world.level.block.Block> blockLookup
+        #endif
     ) {
         CGIdentifier path = CGIdentifier.fromMC(GAMETEST_STRUCTURE_LISTER.idToFile(id));
-        Optional<Resource> resource = this.resourceManager.getResource(path.toMC());
+        Optional<Resource> resource = resourceManager.getResource(path.toMC());
 
         if (resource.isPresent()) {
             try {
@@ -58,7 +67,18 @@ public abstract class StructureTemplateManagerMixin$GameTest {
                     resource.orElseThrow().openAsReader()
                 );
                 CompoundTag nbt = NbtUtils.snbtToStructure(snbt);
-                return Optional.of(this.readStructure(nbt));
+
+                StructureTemplate structureTemplate =
+                    #if MC>=260100
+                    new StructureTemplate();
+                    #else
+                    this.readStructure(nbt);
+                    #endif
+                #if MC>=260100
+                int version = NbtUtils.getDataVersion(nbt, 500);
+                structureTemplate.load(blockLookup, DataFixTypes.STRUCTURE.updateToCurrentVersion(dataFixer, nbt, version));
+                #endif
+                return Optional.of(structureTemplate);
             } catch (IOException | CommandSyntaxException e) {
                 throw new RuntimeException("Failed to load GameTest structure " + id, e);
             }
@@ -75,7 +95,11 @@ public abstract class StructureTemplateManagerMixin$GameTest {
         #else
         ResourceLocation
         #endif
-    > cobblegen$listSnbtStructures() {
+    > cobblegen$listSnbtStructures(
+        #if MC>=260100
+        ResourceManager resourceManager
+        #endif
+    ) {
         return GAMETEST_STRUCTURE_LISTER.listMatchingResources(resourceManager).keySet().stream().map(GAMETEST_STRUCTURE_LISTER::fileToId);
     }
 
@@ -110,12 +134,12 @@ public abstract class StructureTemplateManagerMixin$GameTest {
             new net.minecraft.world.level.levelgen.structure.templatesystem.loader.TemplateSource(dataFixer, holderGetter) {
                 @Override
                 public Optional<StructureTemplate> load(net.minecraft.resources.Identifier id) {
-                    return cobblegen$loadSnbtFromResource(id);
+                    return cobblegen$loadSnbtFromResource(id, resourceManager, dataFixer, holderGetter);
                 }
 
                 @Override
                 public Stream<net.minecraft.resources.Identifier> list() {
-                    return cobblegen$listSnbtStructures();
+                    return cobblegen$listSnbtStructures(resourceManager);
                 }
             }
             #else
